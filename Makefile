158da54a87ad8c52d2c9a3268799ad392c51eb36
Xen <Xenacrhibal@gmail.com>
2018-02-18 23:40:27 +0300

    Hello World

---
+++ Makefile
@@ -0,0 +1,4 @@
+all:
+	g++ -Wall -ggdb3 helloworld.cpp -o helloworld
+clean:
+	rm -rf helloworld
---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main (int argc, char **argv){
+
+    const std::string sHelloWorld = {"Hello, World!"};
+
+    std::cout << sHelloWorld << std::endl;
+    
+    return 0;
+}
